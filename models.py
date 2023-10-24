dedac55fb5e1919e3939546808b478b88b5a08f9
Tejas Agrawal <98448186+Tej-55@users.noreply.github.com>
2023-10-24 18:27:39 +0530

    Update models.py

--- models.py
+++ models.py
@@ -96,16 +96,55 @@ class LayerNorm(nn.Module):
         x = (x - u) / torch.sqrt(s + self.variance_epsilon)
         return self.gamma * x + self.beta
 
-class MultiHeadedSelfAttention(nn.Module):
-    """ Multi-Headed Dot Product Attention """
-    def __init__(self, cfg):
+class QKVAttention_cross(nn.Module):
+    """ QKV Attention """
+    def __init__(self, cfg, heads):
         super().__init__()
-        self.proj_q = nn.Linear(cfg.hidden, cfg.hidden)
-        self.proj_k = nn.Linear(cfg.hidden, cfg.hidden)
-        self.proj_v = nn.Linear(cfg.hidden, cfg.hidden)
+        self.proj_q = nn.Linear(cfg.D, cfg.D)     #self.proj_q = nn.Linear(cfg.hidden, cfg.hidden)
+        self.proj_k = nn.Linear(cfg.C, cfg.D)
+        self.proj_v = nn.Linear(cfg.C, cfg.D)
         # self.drop = nn.Dropout(cfg.p_drop_attn)
         self.scores = None # for visualization
-        self.n_heads = cfg.n_heads
+        self.n_heads = heads
+
+    def forward(self, x, latent, mask):
+        """
+        x, q(query), k(key), v(value) : (B(batch_size), S(seq_len), D(dim))
+        mask : (B(batch_size) x S(seq_len))
+        * split D(dim) into (H(n_heads), W(width of head)) ; D = H * W
+        """
+        # (B, S, D) -proj-> (B, S, D) -split-> (B, S, H, W) -trans-> (B, H, S, W)
+        q, k, v = self.proj_q(latent), self.proj_k(x), self.proj_v(x)
+        #print(x.shape, q.shape)
+        #print(latent.shape, v.shape)
+        q, k, v = (split_last(x, (self.n_heads, -1)).transpose(-2, -3)
+                   for x in [q, k, v])
+        # (B, H, S, W) @ (B, H, W, S) -> (B, H, S, S) -softmax-> (B, H, S, S)
+        scores = q @ k.transpose(-2, -1) / np.sqrt(k.size(-1))
+        if mask is not None:
+            mask = mask[:, None, None, :].float()
+            scores -= 10000.0 * (1.0 - mask)
+        #scores = self.drop(F.softmax(scores, dim=-1))
+        scores = F.softmax(scores, dim=-1)
+        # (B, H, S, S) @ (B, H, S, W) -> (B, H, S, W) -trans-> (B, S, H, W)
+        #print(scores.shape, v.shape)
+        h = (scores @ v).transpose(1, 2).contiguous()
+        #print(h.shape)
+        # -merge-> (B, S, D)
+        h = merge_last(h, 2)
+        self.scores = scores
+        return h
+
+class QKVAttention_self(nn.Module):
+    """ QKV Attention """
+    def __init__(self, cfg, heads):
+        super().__init__()
+        self.proj_q = nn.Linear(cfg.D, cfg.D)
+        self.proj_k = nn.Linear(cfg.D, cfg.D)
+        self.proj_v = nn.Linear(cfg.D, cfg.D)
+        # self.drop = nn.Dropout(cfg.p_drop_attn)
+        self.scores = None # for visualization
+        self.n_heads = heads
 
     def forward(self, x, mask):
         """
@@ -115,7 +154,7 @@ class MultiHeadedSelfAttention(nn.Module):
         """
         # (B, S, D) -proj-> (B, S, D) -split-> (B, S, H, W) -trans-> (B, H, S, W)
         q, k, v = self.proj_q(x), self.proj_k(x), self.proj_v(x)
-        q, k, v = (split_last(x, (self.n_heads, -1)).transpose(1, 2)
+        q, k, v = (split_last(x, (self.n_heads, -1)).transpose(-2, -3)
                    for x in [q, k, v])
         # (B, H, S, W) @ (B, H, W, S) -> (B, H, S, S) -softmax-> (B, H, S, S)
         scores = q @ k.transpose(-2, -1) / np.sqrt(k.size(-1))
@@ -132,12 +171,12 @@ class MultiHeadedSelfAttention(nn.Module):
         return h
 
 
-class PositionWiseFeedForward(nn.Module):
+class FeedForward(nn.Module):
     """ FeedForward Neural Networks for each position """
     def __init__(self, cfg):
         super().__init__()
-        self.fc1 = nn.Linear(cfg.hidden, cfg.hidden_ff)
-        self.fc2 = nn.Linear(cfg.hidden_ff, cfg.hidden)
+        self.fc1 = nn.Linear(cfg.D, cfg.ffw)
+        self.fc2 = nn.Linear(cfg.ffw, cfg.D)
         #self.activ = lambda x: activ_fn(cfg.activ_fn, x)
 
     def forward(self, x):
@@ -146,30 +185,59 @@ class PositionWiseFeedForward(nn.Module):
 
 
 class Transformer(nn.Module):
-    """ Transformer with Self-Attentive Blocks"""
+        """ Transformer with QKV Attention Blocks"""
     def __init__(self, cfg):
         super().__init__()
         self.embed = Embeddings(cfg)
         # Original BERT not used parameter-sharing strategies
         # self.blocks = nn.ModuleList([Block(cfg) for _ in range(cfg.n_layers)])
+        self.latents = nn.Parameter(torch.randn(cfg.N, cfg.D))#[None, :,:]
+
+        self.cross_attend_blocks = nn.ModuleList([
+            QKVAttention_cross(cfg, heads = cfg.cross_heads), #PreNorm(cfg.C, QKVAttention(cfg, heads = cfg.cross_heads), context_dim = cfg.C),
+            FeedForward(cfg) #PreNorm(cfg.D, FeedForward(cfg))
+        ])
+        get_latent_attn = lambda: QKVAttention_self(cfg, heads = cfg.latent_heads) # PreNorm(cfg.D, QKVAttention(cfg, heads = cfg.latent_heads))
+        get_latent_ff = lambda: FeedForward(cfg) #PreNorm(cfg.D, FeedForward(cfg))
+        #get_latent_attn, get_latent_ff = map(cache_fn, (get_latent_attn, get_latent_ff))
+
+        self.layers = nn.ModuleList([
+            QKVAttention_self(cfg, heads = cfg.latent_heads),
+            FeedForward(cfg)
+        ])
+        #cache_args = {'_cache': weight_tie_layers}
+
+
 
         # To used parameter-sharing strategies
-        self.n_layers = cfg.n_layers
-        self.attn = MultiHeadedSelfAttention(cfg)
-        self.proj = nn.Linear(cfg.hidden, cfg.hidden)
-        self.norm1 = nn.LayerNorm(cfg.hidden)
-        self.pwff = PositionWiseFeedForward(cfg)
-        self.norm2 = nn.LayerNorm(cfg.hidden)
+        # self.n_layers = cfg.n_layers
+        # self.attn = QKVAttention(cfg)
+        # self.proj = nn.Linear(cfg.hidden, cfg.hidden)
+        self.norm1 = nn.LayerNorm(cfg.D)
+        # self.pwff = PositionWiseFeedForward(cfg)
+        self.norm2 = nn.LayerNorm(cfg.D)
+        self.norm3 = nn.LayerNorm(cfg.D)
+        self.norm4 = nn.LayerNorm(cfg.D)
         # self.drop = nn.Dropout(cfg.p_drop_hidden)
 
-    def forward(self, x, seg, mask):
-        h = self.embed(x, seg)
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None):
+        x = input_ids
+        seg = token_type_ids
+        mask = attention_mask
+        x = self.embed(x, seg)
+        h = x.clone().detach()
+        cross_attn, cross_ff = self.cross_attend_blocks
+        x = self.norm1(cross_attn(h, self.latents, mask = mask) + self.latents)
 
-        for _ in range(self.n_layers):
-            # h = block(h, mask)
-            h = self.attn(h, mask)
-            h = self.norm1(h + self.proj(h))
-            h = self.norm2(h + self.pwff(h))
+        x = self.norm2(cross_ff(x) + x)
 
-        return h
+        self_attn, self_ff = self.layers
+        for _ in range(cfg.process_layers):
+
+            x = self.norm3(self_attn(x, mask = None) + x)
+            x = self.norm4(self_ff(x) + x)
+
+
+
+        return x
 
